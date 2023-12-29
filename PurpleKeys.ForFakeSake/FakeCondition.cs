f27b0991bc479680d41be195e0393051858a6e1d
Paul Mason <paul.mason@d55.co.uk>
2023-12-29 20:12:50 +0000

    Add Xml Documentation FakeCondition.cs

--- PurpleKeys.ForFakeSake/FakeCondition.cs
+++ PurpleKeys.ForFakeSake/FakeCondition.cs
@@ -1,32 +1,30 @@
 ﻿namespace PurpleKeys.ForFakeSake;
 
 /// <summary>
-/// Represents a condition for a FakeSetup that needs to be satisfied for the Fake to be applied 
+///     Represents a condition for a FakeSetup that needs to be satisfied for the Fake to be applied
 /// </summary>
 /// <typeparam name="T"></typeparam>
 public class FakeCondition<T> {
-    
     /// <summary>
-    /// Condition that is always satisfied.
+    ///     Condition that is always satisfied.
     /// </summary>
-    public static readonly FakeCondition<T> Satisfied = new FakeCondition<T>(_ => true);
-    
-    /// <summary>
-    /// Condition that is never satisfied.
-    /// </summary>
-    public static readonly FakeCondition<T> NotSatisfied = new FakeCondition<T>(_ => false);
-    
-    /// <summary>
-    /// Predicate to evaluation if the condition is met or not.
-    /// </summary>
-    public Func<IDictionary<string, object>, bool> Condition { get; }
+    public static readonly FakeCondition<T> Satisfied = new(_ => true);
 
     /// <summary>
-    /// Creates an instance of Fake Condition
+    ///     Condition that is never satisfied.
+    /// </summary>
+    public static readonly FakeCondition<T> NotSatisfied = new(_ => false);
+
+    /// <summary>
+    ///     Creates an instance of Fake Condition
     /// </summary>
     /// <param name="condition">Predicate to evaluate if a FakeSetup should be used or not.</param>
-    public FakeCondition(Func<IDictionary<string, object>, bool>condition)
-    {
+    public FakeCondition(Func<IDictionary<string, object>, bool> condition) {
         Condition = condition;
     }
+
+    /// <summary>
+    ///     Predicate to evaluation if the condition is met or not.
+    /// </summary>
+    public Func<IDictionary<string, object>, bool> Condition { get; }
 }

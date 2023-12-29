543e4ca3ca634ce9b0b501ccffe941aff5a72a7f
Paul Mason <paul.mason@d55.co.uk>
2023-12-29 20:33:44 +0000

    Add Xml Documentation FakeProperty.cs, IFakingSpy.cs and SpyInterceptor.cs

--- PurpleKeys.ForFakeSake/Spy/IFakingSpy.cs
+++ PurpleKeys.ForFakeSake/Spy/IFakingSpy.cs
@@ -1,11 +1,42 @@
 namespace PurpleKeys.ForFakeSake.Spy;
 
-public interface IFakingSpy
-{
+/// <summary>
+///     Spy on invocations against a Fake.
+/// </summary>
+public interface IFakingSpy {
+    /// <summary>
+    ///     Resets recorded invocations.
+    /// </summary>
     void Reset();
+
+    /// <summary>
+    ///     Returns all invocations recorded by the spy.
+    /// </summary>
+    /// <returns>Returns all invocations recorded by the spy.</returns>
     IEnumerable<SpyInvocation> AllInvocations();
+
+    /// <summary>
+    ///     Returns all invocations recorded by the spy for <typeparamref name="T" />.
+    /// </summary>
+    /// <returns>Returns all invocations recorded by the spy for <typeparamref name="T" />.</returns>
     IEnumerable<SpyInvocation> AllInvocations<T>();
-    IEnumerable<SpyInvocation> MemberInvocations<T>(string methodName);
-    IEnumerable<SpyInvocation> SignatureInvocations<T>(string methodSignature);
-    
+
+    /// <summary>
+    ///     Returns all invocations recorded by the spy for invocations of a member of <typeparamref name="T" />.
+    /// </summary>
+    /// <param name="memberName">Name of the member to return all invocations of, including all overloads.</param>
+    /// <typeparam name="T">Type declaring the member.</typeparam>
+    /// <returns>Returns all invocations recorded by the spy for invocations of a member of <typeparamref name="T" />.</returns>
+    IEnumerable<SpyInvocation> MemberInvocations<T>(string memberName);
+
+    /// <summary>
+    ///     Returns all invocations recorded by the spy for invocations of a member signature of <typeparamref name="T" />.
+    /// </summary>
+    /// <param name="memberSignature">signature of the member to query</param>
+    /// <typeparam name="T">Type declaring the member.</typeparam>
+    /// <returns>
+    ///     Returns all invocations recorded by the spy for invocations of a member with the
+    ///     signature declared on <typeparamref name="T" />.
+    /// </returns>
+    IEnumerable<SpyInvocation> SignatureInvocations<T>(string memberSignature);
 }

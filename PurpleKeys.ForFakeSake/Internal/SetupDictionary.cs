07eed46fe2c1bb9ac2918623ea461daf0f644219
Paul Mason <109951306+p-mason@users.noreply.github.com>
2023-12-29 13:44:49 +0000

    issue-7_refactor-setup-collection (#8)

---
+++ PurpleKeys.ForFakeSake/Internal/SetupDictionary.cs
@@ -0,0 +1,24 @@
+﻿namespace PurpleKeys.ForFakeSake.Internal;
+
+internal class SetupDictionary : Dictionary<string, List<FakeSetup>>
+{
+    public void AddSetup(string key, FakeSetup setup)
+    {
+        if (!TryGetValue(key, out var items))
+        {
+            items = new List<FakeSetup>();
+            Add(key, items);
+        }
+        items.Add(setup);
+    }
+    
+    public void AddSetups(string key, IEnumerable<FakeSetup> setup)
+    {
+        if (!TryGetValue(key, out var items))
+        {
+            items = new List<FakeSetup>();
+            Add(key, items);
+        }
+        items.AddRange(setup);
+    }
+}

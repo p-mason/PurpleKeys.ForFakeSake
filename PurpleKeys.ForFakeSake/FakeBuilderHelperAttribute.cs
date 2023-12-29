d2f829aff4805ed1c996a8c3b414a013909fd282
Paul Mason <paul.mason@d55.co.uk>
2023-12-29 20:06:51 +0000

    Add Xml Documentation to FakeBuilderHelperAttribute.cs

--- PurpleKeys.ForFakeSake/FakeBuilderHelperAttribute.cs
+++ PurpleKeys.ForFakeSake/FakeBuilderHelperAttribute.cs
@@ -1,5 +1,8 @@
 ﻿namespace PurpleKeys.ForFakeSake;
 
+/// <summary>
+/// Attribute to determine how to setup for a Fake.
+/// </summary>
 [AttributeUsage(AttributeTargets.Method)]
 public class FakeBuilderHelperAttribute : Attribute
 {
